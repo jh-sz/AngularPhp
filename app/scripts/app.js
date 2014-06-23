466424917b821282580b2a72e90825807a7fc9af
jh-sz <jh-sz@github.com>
2014-06-24 00:54:59 +1000

    first commit

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name angularPhpApp
+ * @description
+ * # angularPhpApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('angularPhpApp', []);
